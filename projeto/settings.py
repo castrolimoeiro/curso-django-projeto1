242de09087de9ff2f3f7db12c6919f4ad99f352a
David Castro <castrolimoeiro@gmail.com>
2022-02-08 07:56:55 -0300

    alter

--- projeto/settings.py
+++ projeto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q13(x54$wmfe7!9qfaz&!$#tf94oac1e#%p1w9n17theoy0+jm'
+SECRET_KEY = 'a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
