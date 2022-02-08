3cf4678ac52717a2c3c23cc5f6c85dc5f0590996
David Castro <castrolimoeiro@gmail.com>
2022-02-08 18:45:27 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,9 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 # HTTP request
 def home(request):
-    return HttpResponse('HOME 1')
-    # HTTP RESPONSE
-
-
-def contato(request):
-    return HttpResponse('CONTATO 1')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE 1')
-    # HTTP RESPONSE
+    return render(request, 'recipes/home.html', {
+        'name': 'David Castro',
+    })
+    
