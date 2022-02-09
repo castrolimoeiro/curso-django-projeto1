667d5089372fc1b974e2e04c88cce975b38eccdf
David Castro <castrolimoeiro@gmail.com>
2022-02-09 20:20:37 -0300

    mudancas de layout

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # HTTP request
 def home(request):
-    return render(request, 'recipes/home.html', {
+    return render(request, 'recipes/pages/home.html', {
         'name': 'David Castro',
     })
     
